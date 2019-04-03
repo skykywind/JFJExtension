0eb5ff96e640b4bb1e6bc0323ea8162b2a0d333e
skykywind <824214765@qq.com>
2019-04-03 11:29:17 +0900

    Update to adapt Swift 5

--- JFJExtension/Classes/UIView+Extension.swift
+++ JFJExtension/Classes/UIView+Extension.swift
@@ -10,7 +10,7 @@ import SnapKit
 
 public extension UIView {
     // MARK: - 尺寸相关
-    public var x:CGFloat {
+    var x:CGFloat {
         get {
             return self.frame.origin.x
         }
@@ -19,7 +19,7 @@ public extension UIView {
         }
     }
     
-    public var y: CGFloat {
+    var y: CGFloat {
         get {
             return self.frame.origin.y
         }
@@ -28,7 +28,7 @@ public extension UIView {
         }
     }
     
-    public var width: CGFloat {
+    var width: CGFloat {
         get {
             return frame.size.width
         }
@@ -37,7 +37,7 @@ public extension UIView {
         }
     }
     
-    public var height: CGFloat {
+    var height: CGFloat {
         get {
             return frame.size.height
         }
@@ -46,7 +46,7 @@ public extension UIView {
         }
     }
     
-    public var size: CGSize {
+    var size: CGSize {
         get {
             return frame.size
         }
@@ -55,7 +55,7 @@ public extension UIView {
         }
     }
     
-    public var centerX: CGFloat {
+    var centerX: CGFloat {
         get {
             return center.x
         }
@@ -64,7 +64,7 @@ public extension UIView {
         }
     }
     
-    public var centerY: CGFloat {
+    var centerY: CGFloat {
         get {
             return center.y
         }
@@ -75,13 +75,13 @@ public extension UIView {
     
     // MARK: - 尺寸裁剪相关
     /// 添加圆角  radius: 圆角半径
-    public func addRounded(radius:CGFloat) {
+    func addRounded(radius:CGFloat) {
         self.layer.cornerRadius = radius
         self.layer.masksToBounds = true
     }
     
     /// 添加部分圆角(有问题右边且不了) corners: 需要实现为圆角的角，可传入多个 radius: 圆角半径
-    public func addRounded(radius:CGFloat, corners: UIRectCorner) {
+    func addRounded(radius:CGFloat, corners: UIRectCorner) {
         let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
         let mask = CAShapeLayer()
         mask.path = path.cgPath
@@ -90,7 +90,7 @@ public extension UIView {
     
     // MARK: - 添加边框
     /// 添加边框 width: 边框宽度 borderColor:边框颜色
-    public func addBorder(width : CGFloat, borderColor : UIColor = .black) { // 颜色自己给
+    func addBorder(width : CGFloat, borderColor : UIColor = .black) { // 颜色自己给
         self.layer.borderWidth = width;
         self.layer.borderColor = borderColor.cgColor;
     }
@@ -103,7 +103,7 @@ public extension UIView {
     ///   - opacity: 阴影透明度
     ///   - offset: 阴影偏移量
     ///   - radius: 阴影圆角半径
-    public func addShadow(color: UIColor = .black, opacity: Float = 0.1, offset: CGSize = .zero, radius: CGFloat = 1) {
+    func addShadow(color: UIColor = .black, opacity: Float = 0.1, offset: CGSize = .zero, radius: CGFloat = 1) {
         self.layer.shadowColor = color.cgColor
         self.layer.shadowOpacity = opacity
         self.layer.shadowOffset = offset
@@ -113,7 +113,7 @@ public extension UIView {
     
     // MARK: - 视图控制器
     /// View的视图控制器
-    public var viewController: UIViewController? {
+    var viewController: UIViewController? {
         for view in sequence(first: self.superview, next: { $0?.superview }) {
             if let responder = view?.next {
                 if responder.isKind(of: UIViewController.self){
@@ -125,17 +125,16 @@ public extension UIView {
     }
 }
 
-
 public extension UIView {
     
     @discardableResult
-    public func addTo(_ superView: UIView) -> Self {
+    func addTo(_ superView: UIView) -> Self {
         superView.addSubview(self)
         return self
     }
     
     @discardableResult
-    public func layout(_ snapKitMaker: (ConstraintMaker) -> Void) -> Self {
+    func layout(_ snapKitMaker: (ConstraintMaker) -> Void) -> Self {
         self.snp.makeConstraints { (make) in
             snapKitMaker(make)
         }
@@ -150,7 +149,7 @@ public protocol ViewChainable {}
 public extension ViewChainable where Self: UIView {
 
     @discardableResult
-    public func config(_ config: (Self) -> Void) -> Self {
+    func config(_ config: (Self) -> Void) -> Self {
         config(self)
         return self
     }
